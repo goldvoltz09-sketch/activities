0308934a0fbd13629638b842143da40980af111d
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-11-16 15:13:58 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("Hello Vihaan")
+
+#printing my name
+print("My name is Vihaan")
+
+#printing a fun fact about myself
+print("Ilove playing football")
+
+# printing the sum of 3 and 5
+print("The sum of 3 and 5 is", 3+5)
