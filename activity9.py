165753f629007ffc3fd605a5661bf7c248fa4264
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-11-23 15:25:43 +0530

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+math=int(input("enter your marks in mathematics"))
+english=int(input("enter your marks in english"))
+science=int(input("enter your marks in science"))
+hindi=int(input("enter your marks in hindi"))
+sum=math+hindi+science+english
+print("the sum of all your marks is",sum)
+percentage=(sum/400)*100
+print("percentage marks is",percentage)
