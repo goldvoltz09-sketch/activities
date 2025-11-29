1097eed3ca632bbd06ee9d6217f974415c7eeed3
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-11-29 15:46:58 +0530

    Add files via upload

---
+++ activity19.py
@@ -0,0 +1,11 @@
+percent=int(input("enter the percentage"))
+if percent>=90:
+    print("A grade")
+elif percent>=80:
+    print("B grade")
+elif percent>=70:
+    print("C grade")
+elif percent>=60:
+    print("D grade")
+else:
+    print("F grade")
