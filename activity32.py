15f4e9baaaaf033037141ae878ef43b31969b5ad
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-12-13 15:32:14 +0530

    Add files via upload

---
+++ activity32.py
@@ -0,0 +1,8 @@
+chemistry=int(input("enter chemistry marks"))
+math=int(input("enter math marks"))
+physics=int(input("enter physics marks"))
+b=chemistry+math+physics/3
+c=100
+d=print(b is c)
+if d:=True:
+    print("student has full marks")
