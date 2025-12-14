3f2008b7e892dc3d31386f7f7552b6a4a9e53265
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-12-14 15:46:28 +0530

    Add files via upload

---
+++ activity41.py
@@ -0,0 +1,6 @@
+bankbalance=int(input("enter bank balance"))
+salary=int(input("enter monthly salary"))
+tbank=bankbalance+salary
+print("Total bank balance after salary is:",tbank)
+annual=tbank*12
+print("Annual bank balance is:",annual)
