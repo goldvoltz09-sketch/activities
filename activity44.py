baed300a772ade611382b59c5e6f29c645608708
goldvoltz09-sketch <goldvoltz09@gmail.com>
2025-12-21 15:37:47 +0530

    Add files via upload

---
+++ activity44.py
@@ -0,0 +1,9 @@
+bc=input("bikes or cars?")
+if bc.lower()=="bikes":
+ print("yamaha or harley")
+ if bc.lowe()=="cars":
+    print("audi or bmw")
+ else:
+    print("please choose bikes or cars")
+else:
+ print("please choose bikes or cars")
