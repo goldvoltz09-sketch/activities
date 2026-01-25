422681a233a064df704e90028681d9763085ff7f
goldvoltz09-sketch <goldvoltz09@gmail.com>
2026-01-25 15:46:07 +0530

    Add files via upload

---
+++ activity72.py
@@ -0,0 +1,11 @@
+def weathercondition():
+    
+ print('the weather is pleasant in',spring)
+    
+ print('the weather is the same in', autumn)
+
+spring="autumn"
+
+autumn= spring
+
+weathercondition()
